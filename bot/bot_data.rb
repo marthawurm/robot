363e19281128aac7d10903261cdd74b8f7de11b6
njerichelimo <martha.c.chumo@gmail.com>
2012-11-24 13:13:23 +0300

    added my bot code :)

---
+++ bot/bot_data.rb
@@ -0,0 +1,29 @@
+bot_data = {
+ :presubs   => [
+  ["dont", "don't"],
+  ["youre", "you're"],
+  ["love", "like"]
+ ], 
+ 
+ :responses => {
+  :default   => [
+                "I don't understand.",
+                "What?",
+                "Huh?"
+               ],
+  :greetings => ["Hi. I'm [name]. Want to chat?"],
+  :farewell  => ["Goodbye!"],
+  'hello'    => [
+                  "How's it going?",
+                  "How do you do?"
+             ],
+  'i like *' => [
+                  "Why do you like *?",
+                  "Wow! I like * too!"
+                ],
+ }
+}
+                
+                          
+                           
+                 
